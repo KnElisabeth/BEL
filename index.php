e779c45d0adf3bfa87f1a924ad5911e67551fb04
KnElisabeth <elisabeth.hoarau@kndigit.com>
2019-11-21 15:24:06 +0100

    lasmy ajout

---
+++ index.php
@@ -0,0 +1,25 @@
+<!--<link rel="stylesheet" href="css/style.css">
+<body class="particles">
+    <div id="particles-js"></div>
+    <div id="login">
+        <button class="buttonIndex"><a href="formRegister.php">Inscription</a></button>
+        <button class="buttonIndex"><a href="formConnection.php" >Connexion</a></button>
+    </div>
+
+<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script src="js/particles.js"></script>
+
+</body>-->
+
+<link rel="stylesheet" href="css/style.css">
+<body class="particles">
+    <div id="particles-js"></div>
+    <div class="login">
+        <form action="formRegister.php">
+            <button class="buttonIndex">Inscription</button>
+        </form>
+        <form action="formConnection.php">
+            <button class="buttonIndex">Connexion</button>
+        </form>
+    </div><script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script src="js/particles.js"></script></body>
